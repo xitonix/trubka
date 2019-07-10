6e5d611cf074a282b47f67e411b443c99d2fbc0f
Alex <xitonix.io@gmail.com>
2019-07-10 11:38:08 +1000

    add flags

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module go.xitonix.io/trubka
 
 go 1.12
 
-require github.com/jhump/protoreflect v1.4.4
+require (
+	github.com/jhump/protoreflect v1.4.4
+	github.com/xitonix/flags v0.0.1
+)
--- main.go
+++ main.go
@@ -5,12 +5,23 @@ import (
 	"os"
 
 	"github.com/jhump/protoreflect/dynamic"
+	"github.com/xitonix/flags"
 
 	"go.xitonix.io/trubka/proto"
 )
 
 func main() {
-	loader, err := proto.NewFileLoader("")
+	flags.EnableAutoKeyGeneration()
+	flags.SetKeyPrefix("TBK")
+	protoDir := flags.String("proto-root", "The path to the folder where your *.proto files live.").WithShort("p")
+	_ = flags.StringSlice("kafka-endpoints", "The comma separated list of Kafka endpoints in server:port format.").Required().WithTrimming()
+	_ = flags.Verbosity("The verbosity level of the tool (use -vv for more verbosity).")
+	flags.Parse()
+
+	if protoDir.IsSet() {
+		// TODO
+	}
+	loader, err := proto.NewFileLoader(protoDir.Get())
 	if err != nil {
 		exit(err)
 	}
