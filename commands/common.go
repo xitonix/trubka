c7fca3625d24cf5bacf0d8512e70d4bd9e7e919e
Alex <xitonix.io@gmail.com>
2019-10-07 08:54:32 +1100

    Multi format consumer - Draft

--- commands/common.go
+++ commands/common.go
@@ -3,16 +3,22 @@ package commands
 import (
 	"context"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"os"
 	"os/signal"
+	"path/filepath"
 	"regexp"
 	"strings"
 	"syscall"
+	"time"
 
 	"github.com/dustin/go-humanize"
 	"github.com/gookit/color"
+	"github.com/pkg/errors"
 	"gopkg.in/alecthomas/kingpin.v2"
 
+	"github.com/xitonix/trubka/internal"
 	"github.com/xitonix/trubka/kafka"
 )
 
@@ -82,4 +88,75 @@ func getBrokers(commaSeparated string) []string {
 		brokers[i] = strings.TrimSpace(brokers[i])
 	}
 	return brokers
+}
+
+func getLogWriter(logFile string) (io.Writer, bool, error) {
+	switch strings.TrimSpace(strings.ToLower(logFile)) {
+	case "none":
+		return ioutil.Discard, false, nil
+	case "":
+		return os.Stdout, false, nil
+	default:
+		lf, err := os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)
+		if err != nil {
+			return nil, false, errors.Wrapf(err, "Failed to create: %s", logFile)
+		}
+		return lf, true, nil
+	}
+}
+
+func getTopics(topicMap map[string]string, cp *kafka.Checkpoint) map[string]*kafka.Checkpoint {
+	topics := make(map[string]*kafka.Checkpoint)
+	for topic := range topicMap {
+		topics[topic] = cp
+	}
+	return topics
+}
+
+func closeFile(file *os.File) {
+	err := file.Sync()
+	if err != nil {
+		color.Error.Printf("Failed to sync the file: %s\n", err)
+	}
+	if err := file.Close(); err != nil {
+		color.Error.Printf("Failed to close the file: %s\n", err)
+	}
+}
+
+func getCheckpoint(rewind bool, offsetCheckpoint int64, timeCheckpoint time.Time) *kafka.Checkpoint {
+	cp := kafka.NewCheckpoint(rewind)
+	switch {
+	case offsetCheckpoint != -1:
+		cp.SetOffset(offsetCheckpoint)
+	case !timeCheckpoint.IsZero():
+		cp.SetTimeOffset(timeCheckpoint)
+	}
+	return cp
+}
+
+func getOutputWriters(outputDir string, topics map[string]*kafka.Checkpoint) (map[string]io.Writer, bool, error) {
+	result := make(map[string]io.Writer)
+
+	if internal.IsEmpty(outputDir) {
+		for topic := range topics {
+			result[topic] = os.Stdout
+		}
+		return result, false, nil
+	}
+
+	err := os.MkdirAll(outputDir, 0755)
+	if err != nil {
+		return nil, false, errors.Wrap(err, "Failed to create the output directory")
+	}
+
+	for topic := range topics {
+		file := filepath.Join(outputDir, topic)
+		lf, err := os.OpenFile(file, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)
+		if err != nil {
+			return nil, false, errors.Wrapf(err, "Failed to create: %s", file)
+		}
+		result[topic] = lf
+	}
+
+	return result, true, nil
 }
