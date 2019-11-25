00dee170aff2db39ad6103944d7f664e743e63a3
Alex <xitonix.io@gmail.com>
2019-11-26 10:57:36 +1100

    Optional highlight (#3)
    * Add --color/--no-color flag to the root command
    * Disable --color by default on Windows
    * Colorful error message if enabled

--- internal/color_palette.go
+++ internal/color_palette.go
@@ -3,24 +3,53 @@ package internal
 import "github.com/gookit/color"
 
 var (
-	Yellow = color.Warn.Render
-	Green  = color.Info.Render
-	Bold   = color.Bold.Render
-	Red    = color.Red.Render
+	yellow = color.Warn.Render
+	green  = color.Info.Render
+	err    = color.Error.Render
+	bold   = color.Bold.Render
+	red    = color.Red.Render
 )
 
-// RedIfTrue highlights the input in red, if the evaluation function returns true.
-func RedIfTrue(input interface{}, eval func() bool) string {
-	if eval() {
-		return Red(input)
-	}
-	return color.Normal.Render(input)
+type painter func(a ...interface{}) string
+
+// Yellow returns the input in yellow if coloring is enabled.
+func Yellow(input interface{}, colorEnabled bool) interface{} {
+	return colorIfEnabled(input, yellow, colorEnabled)
 }
 
-// GreenIfTrue highlights the input in green, if the evaluation function returns true.
-func GreenIfTrue(input interface{}, eval func() bool) string {
-	if eval() {
-		return Green(input)
+// Green returns the input in green if coloring is enabled.
+func Green(input interface{}, colorEnabled bool) interface{} {
+	return colorIfEnabled(input, green, colorEnabled)
+}
+
+// Bold returns the input in bold if coloring is enabled.
+func Bold(input interface{}, colorEnabled bool) interface{} {
+	return colorIfEnabled(input, bold, colorEnabled)
+}
+
+// Red returns the input in red if coloring is enabled.
+func Red(input interface{}, colorEnabled bool) interface{} {
+	return colorIfEnabled(input, red, colorEnabled)
+}
+
+// Err returns the input in red background if coloring is enabled.
+func Err(input interface{}, colorEnabled bool) interface{} {
+	return colorIfEnabled(input, err, colorEnabled)
+}
+
+// RedIfTrue highlights the input in red, if coloring is enabled and the evaluation function returns true.
+func RedIfTrue(input interface{}, eval func() bool, colorEnabled bool) interface{} {
+	return colorIfEnabled(input, red, colorEnabled && eval())
+}
+
+// GreenIfTrue highlights the input in green, if coloring is enabled and the evaluation function returns true.
+func GreenIfTrue(input interface{}, eval func() bool, colorEnabled bool) interface{} {
+	return colorIfEnabled(input, green, colorEnabled && eval())
+}
+
+func colorIfEnabled(input interface{}, p painter, colorEnabled bool) interface{} {
+	if colorEnabled {
+		return p(input)
 	}
-	return color.Normal.Render(input)
+	return input
 }
