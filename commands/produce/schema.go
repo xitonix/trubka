805ec2ff1b88da13fb77fbc3ebad96256bde74ab
Alex <xitonix.io@gmail.com>
2020-02-09 12:59:35 +1100

    Recursive schema generation

--- commands/produce/schema.go
+++ commands/produce/schema.go
@@ -1,10 +1,11 @@
 package produce
 
 import (
+	"encoding/json"
 	"fmt"
-	"strings"
 
-	"github.com/golang/protobuf/jsonpb"
+	"github.com/golang/protobuf/protoc-gen-go/descriptor"
+	"github.com/jhump/protoreflect/desc"
 	"gopkg.in/alecthomas/kingpin.v2"
 
 	"github.com/xitonix/trubka/commands"
@@ -40,25 +41,58 @@ func (c *schema) run(_ *kingpin.ParseContext) error {
 		return err
 	}
 
-	marshaller := jsonpb.Marshaler{
-		EnumsAsInts:  true,
-		EmitDefaults: true,
-		Indent:       "  ",
-		OrigName:     false,
-		AnyResolver:  nil,
-	}
-
 	msg, err := loader.Get(c.proto)
 	if err != nil {
 		return err
 	}
-
-	b, err := msg.MarshalJSONPB(&marshaller)
+	mp := make(map[string]interface{})
+	readSchema(mp, "", msg.GetMessageDescriptor())
+	b, err := json.MarshalIndent(mp, " ", "   ")
 	if err != nil {
 		return err
 	}
-
-	fmt.Println(strings.ReplaceAll(string(b), "null", "{}"))
-
+	fmt.Println(string(b))
 	return nil
+}
+
+func readSchema(mp map[string]interface{}, oneOffChoice string, md *desc.MessageDescriptor) {
+
+	for _, field := range md.GetFields() {
+		options := field.GetFieldOptions()
+		if options != nil && options.Deprecated != nil && *options.Deprecated {
+			continue
+		}
+		t := field.GetType()
+		if t == descriptor.FieldDescriptorProto_TYPE_MESSAGE {
+			name := field.GetName()
+			oneOffChoice = chooseOneOff(field.GetOneOf())
+			if oneOffChoice != "" && name != oneOffChoice && field.GetOneOf() != nil {
+				continue
+			}
+			parent := make(map[string]interface{})
+			mp[field.GetName()] = parent
+			readSchema(parent, oneOffChoice, field.GetMessageType())
+		} else {
+			options := field.GetFieldOptions()
+			if options != nil && options.Deprecated != nil && *options.Deprecated {
+				continue
+			}
+			mp[field.GetName()] = field.GetDefaultValue()
+		}
+	}
+}
+
+func chooseOneOff(parent *desc.OneOfDescriptor) string {
+	if parent == nil {
+		return ""
+	}
+	choices := parent.GetChoices()
+	for _, choice := range choices {
+		options := choice.GetFieldOptions()
+		if options != nil && options.Deprecated != nil && *options.Deprecated {
+			continue
+		}
+		return choice.GetName()
+	}
+	return ""
 }
